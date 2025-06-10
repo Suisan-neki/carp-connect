323396c9934e2a66797274920f19c1eb9fb67933
suisan <b241651@hiroshima-u.ac.jp>
2025-06-10 11:19:09 +0900

    依存関係

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    turboMode: false, // Turbopack を無効化
+    turboMode: true, // Turbopack を有効化
   },
   reactStrictMode: true, // React Strict Mode を有効にする（推奨）
 };
