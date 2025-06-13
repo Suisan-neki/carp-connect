9133b979d589e03fee0217aeb9d0c3cdb64c4d7a
suisan <b241651@hiroshima-u.ac.jp>
2025-06-13 17:20:21 +0900

    header書き換え2

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,8 +5,9 @@ export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow pt-16">{children}</main>
       <Footer />
     </div>
   );
 }
+
